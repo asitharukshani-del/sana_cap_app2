f6642d8dada4996043f1b81061b0d51665b0d055
Asitha Rukshani <c.rukshani@sana-commerce.com>
2026-03-06 05:22:04 +0000

    My first stable app

---
+++ srv/MyService.cds
@@ -0,0 +1,11 @@
+//SEGW - define a service - definition // Service definition
+
+
+service Myservice @(path: 'MyService') {
+
+
+    //hello world end point
+    function shalini(anubhav: String) returns String;
+
+
+}
---
+++ srv/MyService.js
@@ -0,0 +1,19 @@
+const cds = require('@sap/cds')
+
+module.exports = class MyService extends cds.ApplicationService { init() {
+
+
+
+this.on ('shalini', async (req) => {
+    //only focus on business logic
+    const userName = req.data.anubhav;
+    return "Hey Amigio !! Welcome " + userName;
+
+
+  })
+
+
+
+
+  return super.init()
+}}
